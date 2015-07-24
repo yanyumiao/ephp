1e98f6975910d95b5323cfaf8490501d73a30512
YanYuMiao <357596508@qq.com>
2015-07-25 02:49:54 +0800

    code rule

--- app/config.php
+++ app/config.php
@@ -1,4 +1,5 @@
 <?php 
+
 $GLOBALS['conf']['mysql']=array();
 $GLOBALS['conf']['redis']=array(
 	'host'=>'127.0.0.1',
--- sys/function.php
+++ sys/function.php
@@ -1,17 +1,13 @@
 <?php
+
 // 自动加载
-// 优化
 function autoload($class){
 	require_once MODEL_PATH.$class.'.php';
 }
 
 // 返回一个模型
-// 优化
 function M($class){
 	require_once MODEL_PATH.$class.'.php';
 	return new $class;
 }
 
-function R(){
-	
-}
