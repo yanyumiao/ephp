f0a23d972e4b23fb7ae8d4bc977a951974361fc7
YanYuMiao <357596508@qq.com>
2015-07-25 03:43:44 +0800

    comment return model

--- sys/function.php
+++ sys/function.php
@@ -1,11 +1,10 @@
 <?php
 
-// 自动加载
 function autoload($class){
 	require_once MODEL_PATH.$class.'.php';
 }
 
-// 返回一个模型
+// return a model
 function M($class){
 	require_once MODEL_PATH.$class.'.php';
 	return new $class;
