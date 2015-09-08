205542c35f9baf9c3fa622251d2bf4b18a2fa94e
yanyumiao <357596508@qq.com>
2015-09-08 11:49:56 +0800

    add mysql config

--- app/config.php
+++ app/config.php
@@ -1,6 +1,12 @@
 <?php 
 
-$GLOBALS['conf']['mysql']=[];
+$GLOBALS['conf']['mysql']=[
+	'host'=>'127.0.0.1',
+	'user'=>'root',
+	'password'=>'',
+	'dbname'=>'ephp'
+];
+
 $GLOBALS['conf']['redis']=[
 	'host'=>'127.0.0.1',
 	'port'=>'6379'
