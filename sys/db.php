55460977ef793e73ea4f076041ef47513ad39196
yanyumiao <357596508@qq.com>
2016-07-10 22:39:57 +0800

    Design the DB class

--- sys/db.php
+++ sys/db.php
@@ -1,3 +1,33 @@
 <?php 
 
-class DB {}
+class DB {
+	public static $instance=[];
+
+	public static function getInstance($config=''){
+
+	}
+
+	public static function insert($table, $data, $config=''){
+
+	}
+
+	public static function update($table, $filed, $where, $config=''){
+
+	}
+
+	public static function select($sql, $config=''){
+
+	}
+
+	public static function find($sql, $config=''){
+
+	}
+
+	public static function exec($sql, $cnofig=''){
+
+	}
+
+	public static function query($sql, $config=''){
+
+	}
+}
