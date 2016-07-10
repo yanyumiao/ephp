f815fa5507ea714f111bc839f5ceb87e9befa578
yanyumiao <357596508@qq.com>
2016-07-10 23:16:53 +0800

    db $config=[]

--- sys/db.php
+++ sys/db.php
@@ -1,33 +1,32 @@
 <?php 
-
 class DB {
 	public static $instance=[];
 
-	public static function getInstance($config=''){
+	public static function getInstance($config=[]){
 
 	}
 
-	public static function insert($table, $data, $config=''){
+	public static function insert($table, $data, $config=[]){
 
 	}
 
-	public static function update($table, $filed, $where, $config=''){
+	public static function update($table, $filed, $where, $config=[]){
 
 	}
 
-	public static function select($sql, $config=''){
+	public static function select($sql, $config=[]){
 
 	}
 
-	public static function find($sql, $config=''){
+	public static function find($sql, $config=[]){
 
 	}
 
-	public static function exec($sql, $cnofig=''){
+	public static function exec($sql, $cnofig=[]){
 
 	}
 
-	public static function query($sql, $config=''){
+	public static function query($sql, $config=[]){
 
 	}
 }
