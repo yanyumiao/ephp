635e7d2c0efd21c0406e1df3bbe31d065614b3dc
yanyumiao <357596508@qq.com>
2016-07-12 18:22:01 +0800

    add redis singleton pattern

--- sys/db.php
+++ sys/db.php
@@ -1,13 +1,23 @@
 <?php 
 
 class DB {
-	public static $mysql_db;
-	public static $redis_db;
+	public static $mysql_instance;
+	public static $redis_instance;
 
-	public static function getMysqlDb($config=[]){
+	public static function getMysqlInstance($config=[]){
 		if(empty($config)) $config=$GLOBALS['config']['mysql'];
-		if(empty(self::$mysql_db[$config['dsn']])) self::$mysql_db[$config['dsn']] = new PDO($config['dsn'], $config['user'], $config['password']);
-		return self::$mysql_db[$config['dsn']];
+		if(empty(self::$mysql_instance[$config['dsn']])) self::$mysql_instance[$config['dsn']] = new PDO($config['dsn'], $config['user'], $config['password']);
+		return self::$mysql_instance[$config['dsn']];
+	}
+
+	public static function getRedisInstance($config=[]){
+		if(empty($config)) $config=$GLOBALS['config']['redis'];
+		if(empty(self::$redis_instance[$config['host']])){
+			$redis=new Redis();
+			$redis->connect($config['host'], $config['port']);
+			self::$redis_instance[$config['host']]=$redis;
+		}
+		return self::$redis_instance[$config['host']];
 	}
 
 	public static function insert($table, $data, $config=[]){
