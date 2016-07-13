bc5e37770d17fd5f6df6d34de612d2caef3649e1
yanyumiao <357596508@qq.com>
2016-07-13 14:47:37 +0800

    add placeholder function

--- sys/function.php
+++ sys/function.php
@@ -5,3 +5,7 @@ function autoload($class){
 	if(file_exists(PATH_MODEL.$class.'.php')) require_once PATH_MODEL.$class.'.php';
 }	
 
+function placeholder($num){
+	for($i=0;$i<$num;$i++) { if($i==0) $str='?'; else $str.=',?'; }
+	return $str;
+}
