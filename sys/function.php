331d554fe9919de587a6c5b9927a1ded884e55eb
yanyumiao <357596508@qq.com>
2016-07-15 13:07:24 +0800

    del function placeholder, so that db class needn't depend on others files

--- sys/function.php
+++ sys/function.php
@@ -5,7 +5,3 @@ function autoload($class){
 	if(file_exists(PATH_MODEL.$class.'.php')) require_once PATH_MODEL.$class.'.php';
 }	
 
-function placeholder($num){
-	for($i=0;$i<$num;$i++) { if($i==0) $str='?'; else $str.=',?'; }
-	return $str;
-}
