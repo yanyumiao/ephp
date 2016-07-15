387dffc4373461e441d8a781e8539ca557185bbe
yanyumiao <357596508@qq.com>
2016-07-15 13:13:49 +0800

    write code tight

--- sys/db.php
+++ sys/db.php
@@ -25,7 +25,7 @@ class DB {
 		$db=self::getMysqlInstance($config);
 		$fields=array_keys($data);
 		$values=array_values($data);
-		$sql = 'INSERT INTO '.$table.'('.implode(',', $fields).') VALUES('.trim(str_repeat('?,', count($values)), ',').')';
+		$sql='INSERT INTO '.$table.'('.implode(',',$fields).') VALUES('.trim(str_repeat('?,',count($values)),',').')';
 		$pre=$db->prepare($sql);
 		return $pre->execute($values);
 	}
@@ -34,7 +34,7 @@ class DB {
 		$db=self::getMysqlInstance($config);
 		$sql="UPDATE $table SET ";
 		foreach($data as $k => $v){$sql.="$k=?,";$values[]=$v;}
-		$sql=rtrim($sql, ',')." WHERE";
+		$sql=rtrim($sql, ',').' WHERE';
 		foreach($where as $k => $v){$sql.=" $k=? AND";$values[]=$v;}
 		$sql=rtrim($sql, ' AND');
 		$pre=$db->prepare($sql);
