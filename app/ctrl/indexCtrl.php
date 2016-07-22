576ad48428989f8f3b752c5a4aa6eadc3d1e4f1c
yanyumiao <357596508@qq.com>
2016-07-22 18:45:24 +0800

    a example for ctrl

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -2,12 +2,6 @@
 
 class indexCtrl extends ctrl {
 	public function indexAct(){
-		echo 'Welcome to Ephp';
-	}
-
-	public function testAct(){
-		//echo 'Test';
-		var_dump(CURRENT_TIME);
-		write_log(PATH_LOG.'a.txt', "this is a test\r\n");
+		echo 'Welcome to easyphp';
 	}
 }
