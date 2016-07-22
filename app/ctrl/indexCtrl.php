b49fe3c0d4c1d0b5a55aa43be37aadbeb984458f
yanyumiao <357596508@qq.com>
2016-07-22 15:03:34 +0800

    Add dir app/service app/model app/log

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -4,4 +4,10 @@ class indexCtrl extends ctrl {
 	public function indexAct(){
 		echo 'Welcome to Ephp';
 	}
+
+	public function testAct(){
+		//echo 'Test';
+		var_dump(CURRENT_TIME);
+		write_log(PATH_LOG.'a.txt', "this is a test\r\n");
+	}
 }
