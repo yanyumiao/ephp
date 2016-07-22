fabf559dce67f792ab6619008ecf07c4f7c1c970
yanyumiao <357596508@qq.com>
2016-07-22 15:17:14 +0800

    Add write_log function

--- sys/function.php
+++ sys/function.php
@@ -5,3 +5,8 @@ function autoload($class){
 	if(file_exists(PATH_MODEL.$class.'.php')) require_once PATH_MODEL.$class.'.php';
 }	
 
+function write_log($file, $content){
+	$content='['.date('Y-m-d H:i:s', CURRENT_TIME).'] '.$content;
+	file_put_contents($file, $content, FILE_APPEND|LOCK_EX);
+}
+
