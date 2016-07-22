d8b717e57b8a3774f424173215664f65c85ed6bc
yanyumiao <357596508@qq.com>
2016-07-22 11:22:00 +0800

    Add const CURRENT_TIME

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ define('PATH_MODEL', PATH_APP.'model/');
 define('PATH_CTRL', PATH_APP.'ctrl/');
 
 //
+define('CURRENT_TIME', time());
+
+//
 require PATH_APP.'config.php';
 require PATH_SYS.'app.php';
 require PATH_SYS.'function.php';
