4fb16dff5bf94c35f10639356307b7937cb93414
yanyumiao <357596508@qq.com>
2016-08-04 18:22:18 +0800

    use smarty as view 2

--- sys/ctrl.php
+++ sys/ctrl.php
@@ -1,5 +1,13 @@
 <?php
 
 class ctrl {
-
+	protected function assign($k, $v){
+		$smarty=smartyLib::getInstance();
+		$smarty->assign($k, $v);
+	}
+	
+	protected function display($view){
+		$smarty=smartyLib::getInstance();
+		$smarty->display($view);
+	}
 }
