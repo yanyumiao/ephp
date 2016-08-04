012cf734148e696c6c4d525cf487737ec9cd8148
yanyumiao <357596508@qq.com>
2016-08-04 18:37:57 +0800

    add comment for how to use view with smarty

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -3,6 +3,11 @@
 class indexCtrl extends ctrl {
 	public function indexAct(){
 		echo 'Easy PHP';
+
+
+		//smarty example
+		//move smarty libs to ~/app/lib/smarty/
+
 		//$this->assign('index', 'Easy PHP');
 		//$this->display('index.html');
 	}
